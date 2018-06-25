e206abfa32927ac51247066e8c2663c243bd4fa1
Ammar Kurabi <kurabi@gmail.com>
2018-06-25 23:26:16 +0530

    Start adding the Passport features: End of Lecture 21.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+// const keys = require("./config/keys");
+
 const app = express();
+
+passport.use(new GoogleStrategy({}));
+// appid: emaily-dev-2018--june-208308
 
 app.get("/", (req, res) => {
   res.send({ bye: "buddy" });
